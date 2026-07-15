64a5f6115f10a70074827edc267881f9c5f6d5ed
Naka Rafilipomanana <naka.rafilipomanana@fms-ea.com>
2026-07-15 15:50:38 +0200

    Add dice_roll function, dict_distance_speed dictionary.

--- simulateur-course.py
+++ simulateur-course.py
@@ -1,18 +1,34 @@
+import random
+
+minimum_integer=1
+maximum_integer=6
+
+def dice_roll():
+    dice = random.randint(minimum_integer, maximum_integer)
+    return dice
+
 if __name__ == '__main__':
 
-grid_speed_dice = [["","[1]", "[2]", "[3]", "[4]", "[5]", "[6]"],
-                   [0, 0, 1, 1, 1, 2, 2],
-                   [1, 0, 0, 1, 1, 1, 2],
-                   [2, 0, 0, 1, 1, 1, 2],
-                   [3, -1, 0, 0, 1, 1, 1],
-                   [4, -1, 0, 0, 0, 1, 1],
-                   [5, -2, -1, 0, 0, 0, 1],
-                   [6, -2, -1, 0, 0, 0, "DQ"]]
+    grid_speed_dice = [["","[1]", "[2]", "[3]", "[4]", "[5]", "[6]"],
+                       [0, 0, 1, 1, 1, 2, 2],
+                       [1, 0, 0, 1, 1, 1, 2],
+                       [2, 0, 0, 1, 1, 1, 2],
+                       [3, -1, 0, 0, 1, 1, 1],
+                       [4, -1, 0, 0, 0, 1, 1],
+                       [5, -2, -1, 0, 0, 0, 1],
+                       [6, -2, -1, 0, 0, 0, "DQ"]]
+
+    dict_distance_speed = {0: 0, 1: 23, 2: 46, 3: 69, 4: 92, 5: 115, 6:138}
 
     while True:
         try:
-            nb_horses = input("Nombre de chevaux : ")
+            nb_horses = int(input("Nombre de chevaux : "))
             race_type = input("Type de la course : ")
         except ValueError:
             print("Entrer les données.")
             continue
+
+
+        print(f"Nombre de chevaux : {nb_horses}")
+        print(f"Type de la course : {race_type}")
+
